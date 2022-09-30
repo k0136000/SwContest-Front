88f9291a30eafd3ba58428f24db8db234c554585
khs200 <113793971+khs200@users.noreply.github.com>
2022-09-30 21:41:08 +0900

    sugj

--- swcontest/src/balaok/dld.jsx
+++ swcontest/src/balaok/dld.jsx
@@ -5,19 +5,29 @@ import imC from "./black.png"
 function App() {
   return (
     <>
+    <ul>
    <img className='op' src={imC} ></img>
    <div className='ek'>
     봐라오케
   </div>
+  <li>
   <input className='eamp' type={'eamil'} placeholder = '이메일 주소'></input>
+  </li>
 
-  <input className='pass' type={'password'} placeholder ='비밀번호' ></input>
+  <li>
+    <input className='pass' type={'password'} placeholder ='비밀번호' ></input>
+  </li>
 
+  <li>
   <input className='word'
   type={'password'} placeholder ='비밀번호 다시입력'></input>
-  
-<button className='nemp'>회원가입 완료</button>
+  </li>
+    
+    <li>
+    <button className='nemp'>회원가입 완료</button>
+    </li>
 
+</ul>
     </>
   );
 }
