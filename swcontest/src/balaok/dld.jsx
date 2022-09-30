03742992157fa51b397f656bbedfd19516864a7a
khs200 <113793971+khs200@users.noreply.github.com>
2022-09-30 17:55:10 +0900

    br

---
+++ swcontest/src/balaok/dld.jsx
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './index.css';
+import imC from "./black.png"
+
+function App() {
+  return (
+    <>
+   <img className='op' src={imC} ></img>
+   <div className='ek'>
+    봐라오케
+  </div>
+  <input className='eamp' type={'eamil'} placeholder = '이메일 주소'></input>
+
+  <input className='pass' type={'password'} placeholder ='비밀번호' ></input>
+
+  <input className='word'
+  type={'password'} placeholder ='비밀번호 다시입력'></input>
+  
+<button className='nemp'>회원가입 완료</button>
+
+    </>
+  );
+}
+
+
+export default App;
