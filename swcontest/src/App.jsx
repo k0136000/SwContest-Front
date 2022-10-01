f5ae307ec9bc0704a9771230191de2c8459599a0
sosow0212 <sosow0212@naver.com>
2022-10-01 23:15:14 +0900

    feat: setting project

---
+++ swcontest/src/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { Routes, Route } from "react-router-dom";
+import styled from "styled-components";
+
+function App() {
+  return (
+    <div>
+      settings
+    </div>
+  );
+}
+
+
+export default App;
